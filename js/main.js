a7451cbf8c838b088ab33473da8f4511dcb934d1
Gentim Dao <befrontender@gmail.com>
2017-01-18 15:28:18 +0700

    First working variant a search form without search by ingridients

--- js/main.js
+++ js/main.js
@@ -1,33 +1,30 @@
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('.popup-with-form').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#name',
+
+    // When elemened is focused, some mobile browsers in some cases zoom in
+    // It looks not nice, so we disable it:
+    callbacks: {
+      beforeOpen: function() {
+        if($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#search-field';
+        }
+      }
+    }
+  });
+});
+
 var dropDownMenu = document.getElementById('drop-down-top-menu');
 function toggle(){
   dropDownMenu.classList.toggle('hidden');
   dropDownMenu.classList.toggle('visible');
 }
 document.querySelector('#menu-btn').addEventListener("click", toggle)
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-})
-
-$(document).ready(function() {
-	$('.popup-with-form').magnificPopup({
-		type: 'inline',
-		preloader: false,
-		focus: '#name',
-
-		// When elemened is focused, some mobile browsers in some cases zoom in
-		// It looks not nice, so we disable it:
-		callbacks: {
-			beforeOpen: function() {
-				if($(window).width() < 700) {
-					this.st.focus = false;
-				} else {
-					this.st.focus = '#name';
-				}
-			}
-		}
-	});
-});
 
 function stickIngr(){
 var a = document.querySelector('#ingridients'), b = null, P = 20;
--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,27 @@
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('.popup-with-form').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#name',
+
+    // When elemened is focused, some mobile browsers in some cases zoom in
+    // It looks not nice, so we disable it:
+    callbacks: {
+      beforeOpen: function() {
+        if($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#search-field';
+        }
+      }
+    }
+  });
+});
+
 var dropDownMenu = document.getElementById('drop-down-top-menu');
 function toggle(){
   dropDownMenu.classList.toggle('hidden');
   dropDownMenu.classList.toggle('visible');
 }
 document.querySelector('#menu-btn').addEventListener("click", toggle)
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-})
-
-$(document).ready(function() {
-	$('.popup-with-form').magnificPopup({
-		type: 'inline',
-		preloader: false,
-		focus: '#name',
-
-		// When elemened is focused, some mobile browsers in some cases zoom in
-		// It looks not nice, so we disable it:
-		callbacks: {
-			beforeOpen: function() {
-				if($(window).width() < 700) {
-					this.st.focus = false;
-				} else {
-					this.st.focus = '#name';
-				}
-			}
-		}
-	});
-});
